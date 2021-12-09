af086258c1da4f685101cef454f899d76d1fcd17
DylanToporek <dylantoporek@gmail.com>
2021-12-09 11:30:54 -0500

    commit tree 829d9078cc5bb469c8c5fffa419dfdf2242c22ab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
 require 'pry'
 
 class String
-
+  attr_accessor :string
   def sentence?
-    
+    self.end_with?(".") 
   end
 
   def question?
-
+    self.end_with?("?") 
   end
 
   def exclamation?
-
+    self.end_with?("!") 
   end
 
   def count_sentences
-
+    self.split(/(\?|\.|!)/).filter{|sentance| sentance.length > 1}.length
   end
+  
 end
